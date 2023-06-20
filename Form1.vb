c9edf7f6d1fa34eefc28618efef5b3e6fcd5e4ac
Thomas Le Puil <thomas.lepuil@gmail.com>
2023-06-20 15:36:08 +0200

    Ajoutez des fichiers projet.

---
+++ Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim LeNombre As Integer
+        Dim NbCoups As Integer = 0
+        Randomize()
+        LeNombre = Int((Rnd() * 100) + 1)
+        'MessageBox.Show(LeNombre)
+        Dim laReponse As Integer
+        Do While laReponse <> LeNombre
+            laReponse = InputBox("Quel est le nombre mystère ?", "Jeu Plus ou Moins")
+            NbCoups += 1
+
+            If laReponse > LeNombre Then
+                MessageBox.Show("Plus petit !")
+            ElseIf laReponse < LeNombre Then
+                MessageBox.Show("Plus grand !")
+            End If
+
+        Loop
+
+        'Si le nombre a été trouvé
+        MessageBox.Show("Bravo, Tu as trouvé le nombre mystère en " & NbCoups & " coups")
+
+        'MessageBox.Show(LeNombre)
+        'MessageBox.Show(laReponse)
+    End Sub
+End Class
